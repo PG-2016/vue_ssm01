4c52e3e3df0ddc049ae7fab4be459ba824f39be2
PN <289278769@qq.com>
2020-10-19 19:59:52 +0800

    完成登录

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="login-wrap">
-    
     <el-form
       class="login-form"
       :label-position="labelPosition"
@@ -14,43 +13,73 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-button" type="primary">登录</el-button>
+      <el-button @click.prevent="login()" class="login-button" type="primary"
+        >登录</el-button
+      >
     </el-form>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        // 顶部对齐
-        labelPosition: 'top',
-        formdata: {
-          username: '',
-          password: ''
-        }
-      };
-    }
+  data() {
+    return {
+      // 顶部对齐
+      labelPosition: "top",
+      formdata: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async login() {
+      console.log(this);
+      //   this.$http.post("login",this.formdata).then((res) => {
+      //       console.log(res);
+      //   })
+      const res = await this.$http.post("login", this.formdata);
+      console.log(res);
+      const {
+        data: {
+          data,
+          meta: { msg, status },
+        },
+      } = res;
+      if (status === 200) {
+        this.$router.push({
+            name: "Home"
+        })
+      } else {
+        // element消息提示
+        this.$message({
+          message: "登录失败，账号信息错误！",
+          type: "warning",
+        });
+        console.log("error!");
+      }
+    },
+  },
 };
 </script>
 
 <style>
-.login-wrap{
-    background-color: #324152;
-    /* 垂直居中 */
-    display: flex; 
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+.login-wrap {
+  background-color: #324152;
+  /* 垂直居中 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
-.login-form{
-    background-color: #ffffff;
-    width: 400px;
-    /* 圆角 */
-    border-radius: 5px;
-    padding: 30px;
+.login-form {
+  background-color: #ffffff;
+  width: 400px;
+  /* 圆角 */
+  border-radius: 5px;
+  padding: 30px;
 }
-.login-button{
-    width: 100%;
+.login-button {
+  width: 100%;
 }
 </style>
