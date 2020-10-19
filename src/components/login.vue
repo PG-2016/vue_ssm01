8486fdd8aa68886b93c9b9ce6227496e00805c13
PN <289278769@qq.com>
2020-10-19 15:24:02 +0800

    完成登录表单

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-      login!!!
+  <div class="login-wrap">
+    
+    <el-form
+      class="login-form"
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-button" type="primary">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+      return {
+        labelPosition: 'right',
+        formdata: {
+          username: '',
+          password: ''
+        }
+      };
+    }
+};
 </script>
 
 <style>
-
 </style>
